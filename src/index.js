bd4857b70481a8888d37f16e623adc67f64a1824
cpmssz <ccarla.pms@gmail.com>
2024-07-09 17:45:44 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -4,10 +4,50 @@ function updateWeather(response) {
     console.log(response.data);
     temperatureElement = document.querySelector("#temperature");
     temperature = response.data.main.temp;
+    temperatureElement.innerHTML = Math.round(temperature); // or Math.round(response.data.main.temp). Without the previous step and variable.
+
     cityElement = document.querySelector("#weather-app-city"); // or just #city (week7-Lesson7-typing a city with errors)
     cityElement.innerHTML =  response.data.name; // searchInput.value; 
+
     
-    temperatureElement.innerHTML = Math.round(temperature);
+    let date = new Date(response.data.dt * 1000); // correct way of converting a Unix timestamp date format to time in Javascript;
+    timeElement = document.querySelector("#time");
+    timeElement.innerHTML = formatDate(date);
+
+    function formatDate(date) {
+
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let day = date.getDay();
+
+        let days = [ 
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+        ]
+        
+        let currentDay = days[day]; // or: let day=days[date.getDay()];
+
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+          }
+        
+        return `${currentDay} ${hours}:${minutes}`;
+    }        
+
+    descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+
+    humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.main.humidity}%`;
+
+    
+    windSpeedElement = document.querySelector("#wind");
+    windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
 } 
 
 function searchCity(city) {
