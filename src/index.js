fce0de8d3388a3477afa8fdeb96a9095f0b16b66
cpmssz <ccarla.pms@gmail.com>
2024-07-11 12:52:42 +0100

    Added the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -68,5 +68,49 @@ function handleSearchSubmit(event) {
 }
 
 searchFormElement.addEventListener("click", handleSearchSubmit);
-
 searchCity("Paris");
+
+function displayForecast() {
+    let forecastElement = document.querySelector("#weather-forecast");
+    
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; 
+    let forecastHtml = ""; 
+
+    days.forEach(function (day, index) {
+        if (index < 5) {
+            forecastHtml=  
+                forecastHtml +  
+                ` 
+                <div class="weather-forecast-day"> 
+                    <div class="weather-forecast-date">${day}</div>
+                    <img
+                        src="https://openweathermap.org/img/wn/04d@2x.png"
+                        alt=""
+                        width="45"
+                        class="weather-forecast-icon"
+                     />
+                
+                    <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max">18º</span>
+                        <span class="weather-forecast-temperature-min">12º</span>
+                    </div>
+                </div>
+            `;
+        }
+    })
+
+    /* nota: JS reconhece html - por isso dizemos que é possível injectar html diretamente de JS (JS Template - correct term for this). Por isso não reconhece comments quando os tentamos fazer na parte de html, como os <div>. 
+     Tivemos de incluir a classe na linha 84, para que ficasse tudo na mesma coluna (cada loop). Ou seja, os dados para cada dia. 
+        Isto porque estamos a usar a técnica display flex, que coloca a classe em que a aplicamos, como a linha (equivalente a "row" em Bootstrap - neste caso - a classe "weather-forecast" - html). 
+        E, depois, cada elemento, dentro desta classe, funciona como uma coluna (assim, como temos vários <div> cada um ficaria numa coluna diferente). Para isso, criamos a classe "day" que representa uma coluna (dentro da classe weather-forecast, aqui selecionada através do id).
+        E, colocamos todos os outros elementos, dentro desta classe. Assim, ficam todos dentro desta coluna. 
+        Como são block elements, é criada uma linha antes e após cada um - portanto, ficam em linhas diferentes (dentro da mesma coluna). 
+            Com excepção da img (que é inline). Mas, como está entre 2 block elements fica em linhas diferentes desses 2 <divs>.
+            Neste caso, colocamos as temperaturas max e min (como span, ficando dentro na mesma linha // lado-a-lado). Mas, poderíamos tê-las colocado em div elements. E, depois, do css file, colocávamos a classe superior (forecast-temperatures) como display:flex; justify:center; -> E teria exatamente o mesmo efeito;
+    */ 
+
+    forecastElement.innerHTML = forecastHtml;
+}
+
+displayForecast();
+
